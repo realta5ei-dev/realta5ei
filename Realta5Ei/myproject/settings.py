d1420b1a2c33f2a323fccb510865688ca2e2edc9
realta5ei-dev <realta5ei@gmail.com>
2026-02-05 22:29:00 +0100

    Update settings.py

--- Realta5Ei/myproject/settings.py
+++ Realta5Ei/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-23(o3&hwlmg+cjw$-3o8(jugu4s^ov-)=%yy-n5xtw-gg-*+=9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
